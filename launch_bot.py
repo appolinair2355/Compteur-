2f8fcc03c2c28f7a93517b313c9b6bd500c53dc1
appolinair2355 <felicitationsbien990@gmail.com>
2025-07-24 01:43:54 +0100

    Update launch_bot.py

--- launch_bot.py
+++ launch_bot.py
@@ -5,7 +5,8 @@ Lanceur de bot avec token intégré pour Replit
 import os
 import sys
 import logging
-
+from simple_bot import main
+from simple_web import app
 # Configuration du token
 os.environ["TELEGRAM_BOT_TOKEN"] = "7749786995:AAGr9rk_uuykLLp5g7Hi3XwIlsdMfW9pWFw"
 
